072f7ed7cb38937c517806fc6030fbd2601c244e
GianmarcoRuffi <gianmarco.ruffi@outlook.it>
2022-05-02 17:20:31 +0200

    Aggiunto js

--- script.js
+++ script.js
@@ -6,6 +6,19 @@
 
 const app = new Vue({
   el: "#app",
-  data: {},
+  data: { mailingList: [] },
   methods: {},
+  created() {
+    for (let i = 0; i < 10; i++) {
+      axios
+        .get("https://flynn.boolean.careers/exercises/api/random/mail")
+        .then((res) => {
+          this.mailingList.push(res.data.response);
+          console.log(res.data.response);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  },
 });
